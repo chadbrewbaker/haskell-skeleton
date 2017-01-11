57dcd38d170530816219c9fb6d65b7fe0310be07
Chad Brewbaker <crb002@gmail.com>
2017-01-11 14:55:44 -0600

    playing with roth eqs

--- FinEquations.hs
+++ FinEquations.hs
@@ -4,6 +4,29 @@ interest p r t = p*r*t
 netPresentValue' :: Float -> Float -> Float -> Float
 netPresentValue' r i t = r /  ((1.0 + i ) ** t)
 
+
+-- each year I take a d=$5000 loan to make a contribution
+-- roth earnings compounded at r1
+-- roth loan compounded at r2
+
+rothStep (rothbal, loanbal, contribs,intpaid) contrib r1 r2 = 
+    ( (rothbal + (rothbal * r1) + contrib), (loanbal  + contrib), (contribs + contrib), (intpaid + loanbal * r2) )
+
+
+
+describe (rothbal,  loanbal, contribs, intpaid) = "TaxfreeIncome: " ++ show rothbal ++ "\nPayable can caploss against capgains: " ++show loanbal ++ "\nContribBasis: " ++show contribs ++ "\nInterest deducted from taxed earnings: "++show intpaid
+ --     where
+   --   loss = loanbal - contribs
+     -- earnings = rothbal - contribs
+
+rothSteps years (rothbal, loanbal, contribs, intpaid) contrib r1 r2 = if years == 0 then 
+                                                                (rbalnext, loanbalnext, contribnext, intpaidnext) 
+                                                                else
+                                                                rothSteps (years-1) (rbalnext, loanbalnext, contribnext, intpaidnext) contrib r1 r2
+                where
+                (rbalnext, loanbalnext, contribnext, intpaidnext) = rothStep (rothbal, loanbal, contribs, intpaid) contrib r1 r2 
+
+
 netPresentValue ::  [Float] -> Float -> Float -> Float
 netPresentValue [] i t = 0.0
 netPresentValue (x:xs) i t = netPresentValue' x i t + netPresentValue xs i (t+1.0)
@@ -30,6 +53,7 @@ pairToCSV (a,b) = show a ++ "," ++ show b ++ "\n"
 csv = unlines $ map pairToCSV pairs
 
 main = do
-	    putStrLn "Payment,PaybackPeriod"
-	    putStrLn csv
+        --putStrLn "Payment,PaybackPeriod"
+        --putStrLn csv
+        putStrLn $ describe $ rothSteps 20 (0.0, 0.0,0.0,0.0) 5000.0 0.05 0.05
 
